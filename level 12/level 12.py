0d5b4d534c1abdfd36aa798d26248261907434ee
demetrekochiashvili <demedudu5@gmail.com>
2024-06-24 22:32:49 +0400

    commit

---
+++ level 12/level 12.py
@@ -0,0 +1,8 @@
+
+name = input("enter your name")
+surename = input("enter your surename")
+age = input("enter your age")
+
+print(name)
+print(surename)
+print(age)
---
+++ level 8/homework/homework.py
@@ -0,0 +1,23 @@
+name =  1
+nume = 1
+nam = 1
+nem = 1
+nime = 1
+mume = 1
+neme = 1
+neim =1
+nome = 1
+nema = 1
+
+
+name = "demetre"
+surename = "kochiashvili"
+age = 10
+year = 2013
+living = "dedopli_stskaro"
+
+print(name)
+print(surename)
+print(age)
+print(year)
+print(living)
