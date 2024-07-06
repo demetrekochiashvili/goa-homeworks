7afe8b9cff27fd794c80884888c59d01b38b147e
demetrekochiashvili <demedudu5@gmail.com>
2024-07-06 14:18:28 +0400

    commit

---
+++ level 14/hw.py
@@ -0,0 +1,30 @@
+num = input("enter your name: ")
+num = input("enter your surename: ")
+num = input("enter your age: ")
+print("name")
+print("surename")
+print("age")
+
+
+
+
+str = "name"
+int = 345
+float = 15.32
+print(str) # string = "name"
+print(int) # integer = collabed  number
+print(float) # folat = disintigrated number
+
+
+
+
+
+
+
+num = 253
+num = 345
+print("integer")
+num =(4*4)
+num =(9*2)
+num =(7*7)
+num =(8*8)
