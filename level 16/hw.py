ce29350a62b9e1b658aa94ef941416369dc7b79f
demetrekochiashvili <demedudu5@gmail.com>
2024-07-13 11:53:06 +0400

    commit

---
+++ level 16/hw.py
@@ -0,0 +1,21 @@
+
+
+
+
+print(true or(500>400))
+print(true or(5<9))
+print(false or(13>16))
+print(false or(90<1))
+print(true or (0<1))
+print(false or (700<5))
+print(true or (31>1))
+print(false or(89<8))
+print(false or (8000<1))
+print(true or (40>1))
+print(false or (50<12))
+print(true or (7>1))
+print(false or (400<611))
+print(true or (8>1))
+print(false or (78>12))
+print(true or (9>1))
+print(false or (1<0))
