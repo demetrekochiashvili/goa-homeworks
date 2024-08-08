8f6aa029448a9350dded8a3ae5780ae35e48a52c
demetrekochiashvili <demedudu5@gmail.com>
2024-08-08 13:57:17 +0400

    commit

---
+++ level 24/level. 24/cw.py
@@ -0,0 +1,29 @@
+
+
+
+cunter = 0
+
+while cunter < 99:
+    print(cunter)
+    cunter += 1
+
+    cunter = 10
+
+    while cunter <100:
+       print(cunter)
+       cunter += 1
+
+       cunter = 1
+       while cunter <20:
+           print(cunter)
+           cunter += 10
+
+           cunter = 0
+           while cunter <30:
+               print(cunter)
+               cunter += 3
+
+       cunter=12
+       while cunter <2:
+           print(cunter)
+           cunter += 6
