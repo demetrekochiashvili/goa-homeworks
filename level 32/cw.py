f554c098b55f1646cb73907c703d0d7d5696cba8
demetrekochiashvili <demedudu5@gmail.com>
2024-09-08 11:18:03 +0400

    commit

---
+++ level 32/cw.py
@@ -0,0 +1,32 @@
+
+
+
+list1 =["html", "CSS", "js"]
+print(list1[1] + " " + "is me favorite.")
+
+program = ["python", "html", "c++", "js"]
+program[3] = c#
+print(program)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+name = "demetre"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
