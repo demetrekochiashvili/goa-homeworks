3a4d14dc03c7e0bdc0d9699dee0ff6f282f480df
demetrekochiashvili <demedudu5@gmail.com>
2025-03-06 21:59:34 +0400

    commit

---
+++ level 39/homework/hw.py
@@ -0,0 +1,7 @@
+listn1 = [3,8,8,2,2]
+listn2 = [5,7,4,8,3]
+listn3 = [1,2,6,5,4]
+
+print(max(listn1))
+print(max(listn2))
+print(max(listn3))
---
+++ level 68/cw.py
@@ -0,0 +1,12 @@
+# listn1 = ["apple"]
+# listn1.append("samsung")
+# listn1.append("google")
+# print(listn1)
+
+name = "demetre kotchiasvili" 
+newname = name.replace(" ","")
+
+print(newname)
+
+
+
